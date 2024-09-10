a7c69ab6588c6b9fcd086a92d7b1ae4a42ea91d2
rajatchauhan99 <rajatchauhan99@gmail.com>
2024-09-10 21:36:53 +0530

    new commit

---
+++ src/data/data_ingestion.py
@@ -0,0 +1,8 @@
+
+
+### this would be required in case we need to pull data from some source
+
+## there can be variety of sources like API calls or databases or simply csv files
+
+
+## it can be live streaming data as well
